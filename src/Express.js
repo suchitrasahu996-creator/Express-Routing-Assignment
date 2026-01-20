44b89dba0b50ddf22180e734ea357d11669517e1
suchitrasahu996-creator <suchitrasahu996@gmail.com>
2026-01-20 23:57:55 +0530

    added the answer

--- src/Express.js
+++ src/Express.js
@@ -1,6 +1,14 @@
-const express =require ('express');
-const app= express();
+const express = require("express");
+const userRouter = require("./routes/users.routes");
+const todoRouter = require("./routes/todos.routes");
+
+const app = express();
+
+app.use(express.json());
+app.use("/users",userRouter);
+app.use("/todos",todoRouter);
 const PORT =3000;
-app.listen (PORT,()=>{
+
+app.listen(3000,()=>{
     console.log(`Server is running on http://localhost:3000`);
-})
+});
